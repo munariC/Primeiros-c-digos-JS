575fc5a3cd4fb58b0551bf0b1df699d264766077
munariC <munaricintya@gmail.com>
2022-09-10 10:50:28 -0300

    removendo pasta exercicios

--- exercicios/ex01.js
+++
@@ -1,8 +0,0 @@
-/*criar uma variavel que comece em zero, fazer um laço de repetição somando 1 até que o valor final da variavel seja 10
-e imprima no console apenas o valor final*/
-
-var num = 0
-while (num < 10){
- num++    
-} 
-console.log(`Resultado ${num}`) 
